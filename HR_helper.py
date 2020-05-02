3b93d4e3a78d86fbbd948b914a637f7ed4a82270
Ville Lehtonen <violehtonen@gmail.com>
2020-05-02 11:43:01 +0200

    refactor api and update readme

--- HR_helper.py
+++ HR_helper.py
@@ -5,53 +5,50 @@ import json
 
 app = Flask(__name__)
 
-#TODO:
-# Add error handling (i.e. if key is not in dictionary or it is None etc.)
-# Test with multiple github accounts
-# Host on Heroku
-# IDEA: recruiter helper
-    # -> Find the languages where the candidate is most proficient
-    # -> Find the level of the programmer
-    # -> Find the most important project of the candidate to further look at it
-    # -> Provide some basic information about candidate (i.e. picture etc.)
-    # -> Future ideas: develop algorithms further and add functionalities
-
 
 @app.route('/', methods = ['GET'])
 def get_user():
+    # Handle query parameter (username)
     baseurl = "https://api.github.com/users/"
     username = request.args.get('user')
 
     try:
-        # Get information about the user and his/her (public) repositories
+        # Fetch user and repository information from Github api 
         user_data = requests.get(f"{baseurl}{username}").json()
         repo_data = requests.get(f"{baseurl}{username}/repos?per_page=50").json()
 
-        # Filter only name, github url, # of public repos, and avatar image from the user data
-        result = {i: user_data[i] for i in ('name', 'html_url', 'public_repos', 'avatar_url')}
+        # From user information, filter only relevant information and store it into the result object
+        user_information = {i: user_data[i] for i in ('name', 'html_url', 'public_repos', 'avatar_url')}
+        result = {}
+        result["user_information"] = user_information
     except:
-        #if user was not found, return 404
+        # If user was not found, return Not Found (404)
         abort(404)
 
-    repo_and_languages = getBiggestRepoAndProgrammingLanguagesUsed(repo_data)
-    biggest_repo = repo_and_languages[0]
-    programming_languages = repo_and_languages[1]
+    #Parse repository data and get the biggest repository (in rows of code) and the percentage of different programming languages used
+    biggest_repo_and_languages = getBiggestRepoAndProgrammingLanguagesUsed(repo_data)
+    biggest_repo = biggest_repo_and_languages[0]
+    programming_languages = biggest_repo_and_languages[1]
 
     # Add a 'programmer level' for the user based on the amount of repos
     result['programmer_level'] = defineProgrammerLevel(result)
-    # Add information on biggest repo
-    result["biggest_repo"] = biggest_repo
-    # Add percentages of programming languages used
-    result['percentage_of_programming_languages_used'] = calculatePercentageOfProgrammingLanguagesUsed(programming_languages)
+
+    # Add information about the most significant piece of work (i.e. the biggest repo)
+    result["Most_significant_work"] = biggest_repo
+
+    # Add the percentages of different programming languages used
+    result['Programming languages used (%)'] = calculatePercentageOfProgrammingLanguagesUsed(programming_languages)
     
     return jsonify(result)
 
 
 def defineProgrammerLevel(result):
     """ Takes a json object with a key 'public_repos' and defines the level of the programmer  """
-    if result['public_repos'] >= 40:
+    n_public_repos = result["user_information"]['public_repos']
+
+    if n_public_repos >= 40:
         programmer_level = 'Godlike developer'
-    elif result['public_repos'] >= 20:
+    elif n_public_repos >= 20:
         programmer_level = 'Rising star'
     else:
         programmer_level = 'Beginner :)'
@@ -61,8 +58,11 @@ def defineProgrammerLevel(result):
 
 def calculatePercentageOfProgrammingLanguagesUsed(programming_languages):
     total = sum(programming_languages.values())
-    return {k: v / total for total in (sum(programming_languages.values()),) for k, v in programming_languages.items()}
+    for key in programming_languages.keys():
+        programming_languages[key] = str(round((programming_languages[key] / total) * 100)) + "%"
 
+    return programming_languages
+    
 
 def getBiggestRepoAndProgrammingLanguagesUsed(repo_data):
     # Find repo with biggest size
@@ -85,7 +85,7 @@ def getBiggestRepoAndProgrammingLanguagesUsed(repo_data):
 
     name_of_repo = repo_data[biggest_repo_index]["name"]
     planguage = repo_data[biggest_repo_index]["language"]
-    biggest_repo = {"name" : name_of_repo, "language" : planguage, "size" : biggest_repo_size}
+    biggest_repo = {"Repository name" : name_of_repo, "Programming language used" : planguage, "Project size (in rows)" : biggest_repo_size}
 
     return biggest_repo, programming_languages
 

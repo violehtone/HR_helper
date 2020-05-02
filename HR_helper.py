5063613f5b79f8f191933d4ea2adff1191178681
Ville Lehtonen <violehtonen@gmail.com>
2020-05-02 14:12:37 +0200

    fixing port

--- HR_helper.py
+++ HR_helper.py
@@ -105,4 +105,4 @@ def getBiggestRepoAndProgrammingLanguagesUsed(repo_data):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=port)
